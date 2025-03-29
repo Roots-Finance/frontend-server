140bb8d7d8fdf197d1d8a2892eb7ab1275d62f37
Rocco A <43506958+GenerelSchwerz@users.noreply.github.com>
2025-03-28 23:58:36 -0400

    added basic auth0 based on example, now integrate

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,14 +1,18 @@
-"use client"
+// components/ThemeProvider.tsx
+'use client';
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider {...props}>
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
       {children}
     </NextThemesProvider>
-  )
+  );
 }
 
-// Place this in components/theme-provider.tsx or components/ui/theme-provider.tsx
